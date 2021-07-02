b261e80c327501c449c3e2efe4fe2d819a83e28f
Hanul <hanul@hanul.me>
2021-07-02 10:46:47 +0900

    update for token uri

--- contracts/DogeSoundClubNFT.sol
+++ contracts/DogeSoundClubNFT.sol
@@ -1,6 +1,35 @@
 pragma solidity ^0.5.6;
 
-import "./klaytn-contracts/token/KIP17/KIP17Token.sol";
+import "./klaytn-contracts/token/KIP17/KIP17Full.sol";
+import "./klaytn-contracts/token/KIP17/KIP17Mintable.sol";
+import "./klaytn-contracts/token/KIP17/KIP17Burnable.sol";
+import "./klaytn-contracts/token/KIP17/KIP17Pausable.sol";
 
-contract DogeSoundClubNFT is KIP17Token("Doge Sound Club NFT", "DSC") {
+contract DogeSoundClubNFT is KIP17Full("Doge Sound Club NFT", "DSC"), KIP17Mintable, KIP17Burnable, KIP17Pausable {
+
+    function tokenURI(uint256 tokenId) public view returns (string memory) {
+        require(_exists(tokenId), "KIP17Metadata: URI query for nonexistent token");
+
+        string memory baseURI = "https://api.dogesound.club/nft/";
+
+        string memory idstr;
+        if (tokenId == 0) {
+            return "0";
+        }
+        uint256 temp = tokenId;
+        uint256 digits;
+        while (temp != 0) {
+            digits += 1;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (tokenId != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(tokenId % 10)));
+            tokenId /= 10;
+        }
+        idstr = string(buffer);
+
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, idstr)) : "";
+    }
 }
