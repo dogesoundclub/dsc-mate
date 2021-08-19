89b02bd9fc1c4c3a51ff4fec3b34a27dfba6ab6b
Hanul <hanul@hanul.me>
2021-08-19 15:55:58 +0900

    Safe Math 적용

--- contracts/MateCountTransfer.sol
+++ contracts/MateCountTransfer.sol
@@ -1,8 +1,10 @@
 pragma solidity ^0.5.6;
 
 import "./klaytn-contracts/token/KIP17/IKIP17Enumerable.sol";
+import "./klaytn-contracts/math/SafeMath.sol";
 
 contract MateCountTransfer {
+    using SafeMath for uint256;
 
     IKIP17Enumerable public mate;
 
@@ -13,10 +15,10 @@ contract MateCountTransfer {
     function countTransfer(address to, uint256 start, uint256 count) external {
         uint256 total = mate.totalSupply();
         uint256 _count = 0;
-        for (uint256 id = start; id < total; id += 1) {
+        for (uint256 id = start; id < total; id = id.add(1)) {
             if (mate.ownerOf(id) == msg.sender) {
                 mate.transferFrom(msg.sender, to, id);
-                _count += 1;
+                _count = _count.add(1);
                 if (_count == count) {
                     break;
                 }
